fef1a586305467343c36bceaf3aa8f6ce2ede166
Tommy <91548470+HungDuong01@users.noreply.github.com>
2024-04-01 18:22:13 -0400

    new python code

---
+++ views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, request
+import pymongo as pm
+#from extensions import crimes, crimes_pred
+#from predictions import setup_prediction_model, predict_crime
+from pandas import DataFrame
+from datetime import datetime
+#from graphs import get_plot, scatter_plot
+
+views = Blueprint(__name__, 'views')
+
+#main page
+@views.route('/')
+def index():
+    return render_template('Index.html')
+
