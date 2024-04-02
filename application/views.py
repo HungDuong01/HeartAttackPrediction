61ed75f0bed47653325d640f83e7f8dfa10042dc
Tommy <91548470+HungDuong01@users.noreply.github.com>
2024-04-02 19:47:22 -0400

    push new prediction.py

--- application/views.py
+++ application/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 import pymongo as pm
-#from extensions import crimes, crimes_pred
-#from predictions import setup_prediction_model, predict_crime
+import pandas as pd
+import joblib
+from prediction import setup_prediction_model,predict_heartRisk
 from connection import db
 from pandas import DataFrame
 from datetime import datetime
@@ -52,15 +53,69 @@ def home_page():
 def prediction():
     premise_dict = {
         0: 'Age',
-        1: 'Sex',
-        2: 'Cholesterol',
-        3: 'Heart Rate',
-        4: 'Diabetes',
-        5: 'Smoking',
-        6: 'Obesity',
-        7: 'Alcohol Consumption',
-        8: 'Exercise Hours per Week',
-        9: 'Diet',
-        
+        1: 'Cholesterol',
+        2: 'Heart Rate',
+        3: 'Family History',
+        4: 'Smoking',
+        5: 'Obesity',
+        6: 'Alcohol Consumption',
+        7: 'Exercise Hours per Week',
+        8: 'Previous Heart Problems',
+        9: 'Medication Use',
+        10: 'Stress Level',
+        11: 'BMI',
+        12: 'Physical Activity Days per Week',
+        13: 'Sleep Hours per Day',
+        14: 'Systolic_BP',
+        15: 'Diastolic_BP',
+        16: 'Sex_Cat'
     }
+    if request.method == 'POST':
+        # get premise from form
+        premise = int(request.form['premise'])
+        
+        # get date from form
+        age = request.form['Age']
+        chol = request.form['Cholesterol']
+        heartRate = request.form['Heart Rate']
+        diabetes = request.form['Diabetes']
+        famHistory = request.form['Family Hisory']
+        smoking = request.form['Smoking']
+        obesity = request.form['Obesity']
+        alcoholCon = request.form['Alcohol Consumption']
+        execisePerWeek = request.form['Exercise Hours Per Week']
+        prevHeartProb = request.form['Previous Heart Problems']
+        medication = request.form['Medication Use']
+        stressLev = request.form['Stress Level']
+        bmi = request.form['BMI']
+        physActDays = request.form['Physical Activity Days Per Week']
+        sleepHrs = request.form['Sleep Hours Per Day']
+        sys_BP = request.form['Systolic_BP']
+        dia_BP = request.form['Diastolic_BP']
+        sexCate = request.form['Sex_Cat']
+        
+        # create data frame    
+        user_input = DataFrame({'Age':[age],
+                           'Cholesterol': [chol],
+                           'Heart Rate': [heartRate],
+                           'Diabetes': [diabetes],
+                           'Family History': [famHistory],
+                           'Smoking': [smoking],
+                           'Obesity':[obesity],
+                           'Alcohol Consumption':[alcoholCon],
+                           'Exercise Hours Per Week':[execisePerWeek],
+                           'Previous Heart Problems':[prevHeartProb],
+                           'Medication Use':[medication],
+                           'Stress Level':[stressLev],
+                           'BMI':[bmi],
+                           'Physical Activity Days Per Week':[physActDays],
+                           'Sleep Hours Per Day':[sleepHrs],
+                           'Systolic_BP':[sys_BP],
+                           'Diastolic_BP':[dia_BP],
+                           'Sex_Cat':[sexCate]
+                           })
+           # Create DataFrame from user input
+        user_df = pd.DataFrame([user_input])
+
+    
     return render_template('prediction.html')
