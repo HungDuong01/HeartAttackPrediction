cbc16c2f81ec1ddae3c479c232a7e3a95571970d
Tommy <91548470+HungDuong01@users.noreply.github.com>
2024-04-01 23:13:37 -0400

    updated login and signup

--- application/views.py
+++ application/views.py
@@ -7,23 +7,23 @@ from datetime import datetime
 #from graphs import get_plot, scatter_plot
 
 views = Blueprint('views', __name__)
-
+#login page
 @views.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
        
         return redirect(url_for('views.home_page'))
     return render_template('login.html')
-
+#signup page
 @views.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        # Your signup logic here
-        # On successful signup, redirect to the login page
+        
         return redirect(url_for('views.login'))
-    # Render the signup page for GET requests
+    
     return render_template('signup.html')
+#home page
 @views.route('/home')
 def home_page():
-    # The function name is changed to 'home_page' to avoid conflict with the 'home' variable
+   
     return render_template('index.html')
